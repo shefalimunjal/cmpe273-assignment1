925038e12ece7b9bdf398669e500b36e100a1edf
Shefali Munjal <shefali.jmit@gmail.com>
2017-03-17 17:55:07 -0700

    assignment completed

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,4 @@ COPY . /app
 WORKDIR /app
 RUN pip install -r requirements.txt
 EXPOSE 5000
-ENTRYPOINT ["python"]
-CMD ["app.py"]
+ENTRYPOINT [ "python", "app.py" ]
--- app.py
+++ app.py
@@ -1,42 +1,56 @@
 from flask import Flask
 from sys import argv
-import subprocess
-import os
+import json
+import ruamel.yaml
+from github import Github
+from github import UnknownObjectException
 
 path = argv[1]
-git_dir = "data"
-print path
+url_parts = path.split('/')
+username = url_parts[-2]
+repo_name = url_parts[-1]
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    git_clone = subprocess.Popen(
-            ["git","clone", path, git_dir],
-             stdout = subprocess.PIPE,
-            stderr = subprocess.PIPE)
-    out, error = git_clone.communicate()
+@app.route("/v1/<filename>")
+def home(filename):
+    return get_config(filename)
 
-    os.chdir(git_dir)
-    #retval = os.getcwd()
-    out = ""
-    for filename in os.listdir("."):
+
+def get_file_contents(user,repo,filename):
+    try:
+        github = Github()
+        user = github.get_user(user)
+        repository = user.get_repo(repo)
+        file_contents = repository.get_contents(filename)
+        return file_contents.decoded_content
+    except UnknownObjectException:
+        return None
+
+
+def load_yaml(file_contents):
+    return ruamel.yaml.round_trip_load(file_contents, preserve_quotes=True)
+
+def get_config(filename):
+    try :
         if filename.endswith(".yml"):
-            f = open(filename, 'r')
-            out += "<h2> contents of file " + filename + "</h2>"
-            out += "<ul>"
-            for line in f.readlines():
-                if line != "\n":
-                    out +="<li>" + line + "</li>"
-            out += "</ul>"
-            f.close()
+            file_contents = get_file_contents(username,repo_name,filename)
+            if file_contents is None: return "Could not connect to repository: " + path + ", for file: "+ filename + "\n"
+            obj = load_yaml(file_contents)
+            return ruamel.yaml.round_trip_dump(obj) + "\n"
 
-    return out
+        elif filename.endswith(".json"):
+            filename = filename.replace(".json",".yml")
+            file_contents = get_file_contents(username,repo_name,filename)
+            if file_contents is None: return "Could not connect to repository: " + path + ", for file: "+ filename + "\n"
+            obj = load_yaml(file_contents)
+            return json.dumps(obj) + "\n"
 
-
-
-
-
+        else:
+            return "format not supported \n"
+    except ruamel.yaml.YAMLError as e:
+        return "Parse error. could not parse file: " + filename + "\n"
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
